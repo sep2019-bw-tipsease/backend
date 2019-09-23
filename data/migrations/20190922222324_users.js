022be1840cb9bb8d84da3b5379f1a9d254b25f3e
Connor Claxton <czclaxton@gmail.com>
2019-09-22 22:55:00 -0500

    finished setting up users and workerData tables

--- data/migrations/20190922222324_users.js
+++ data/migrations/20190922222324_users.js
@@ -12,7 +12,21 @@ exports.up = function(knex) {
       users.string("role", 128).notNullable();
       users.string("image", 256);
     })
-    .createTable("workerData", wData => {});
+    .createTable("workerData", wData => {
+      wData
+        .integer("worker_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      wData.string("time", 128).notNullable();
+      wData.string("tagline").notNullable();
+      wData.string("job_title").notNullable();
+      wData.string("company").notNullable();
+      wData.decimal("tip_total");
+    });
 };
 
 exports.down = function(knex) {
