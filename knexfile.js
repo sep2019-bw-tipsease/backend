edc29879ca5164c2605a871f199f224d6be62deb
Connor Claxton <czclaxton@gmail.com>
2019-09-25 12:56:24 -0500

    d

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   production: {
     client: "pg",
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + "/db/migrations"
     },
